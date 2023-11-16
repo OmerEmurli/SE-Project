91b108d1102aab71eee26c774da0ab60f0e12c9f
Omer Emurli <omer123emurli@gmail.com>
2023-11-16 17:57:19 +0100

    new tests

---
+++ build.sbt
@@ -0,0 +1,11 @@
+val scala3Version = "3.3.1"
+
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "battleship2",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := scala3Version,
+    libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.14",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % "test"
+  )
---
+++ src/main/scala/de/htwg/se/battleship2/model/GameBoard.scala
@@ -0,0 +1,37 @@
+case class GameBoard(rows: Int, cols: Int) {
+
+  val eol: String = sys.props("line.separator")
+
+  def border(cols: Int): String = "  X" + "  X" * cols + "  X" + eol
+
+  def letters(cols: Int): String = {
+    val letters = ('A' until ('A' + cols).toChar).map(_.toString).mkString("  ")
+    s"     $letters" + eol
+  }
+
+  def generateBoard(rows: Int, cols: Int): String = {
+    val rowIndices = (0 until rows).map(i => (i + 1).toString)
+    val gameBoardRows = (0 until rows).map { i =>
+      (0 until cols).map(row => "  -").mkString("X", "", "  X")
+    }
+    val boardWithIndices = rowIndices
+      .zip(gameBoardRows)
+      .map { case (idx, row) =>
+        s"$idx $row" + eol
+      }
+      .mkString
+    s"$boardWithIndices"
+  }
+
+  def printBoard(): String =
+    letters(cols) + border(cols) + generateBoard(rows, cols) + border(cols)
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val gameBoard = GameBoard(rows = 5, cols = 5)
+    println("Welcome to Battleship2 \n")
+    println(gameBoard.printBoard())
+
+  }
+}
