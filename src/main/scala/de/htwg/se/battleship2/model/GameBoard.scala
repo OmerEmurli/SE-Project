deef4f6b5a809ffd92c7b86600df8a3ee9d1754e
Omer Emurli <omer123emurli@gmail.com>
2023-11-22 16:47:36 +0100

    Adding an TUI and new classes

--- src/main/scala/de/htwg/se/battleship2/model/GameBoard.scala
+++ src/main/scala/de/htwg/se/battleship2/model/GameBoard.scala
@@ -1,37 +1,30 @@
-case class GameBoard(rows: Int, cols: Int) {
+import scala.io.StdIn.readLine
+import de.htwg.se.battleship2.model.Filled
+import de.htwg.se.battleship2.model.Matrix
+import de.htwg.se.battleship2.model.Field
 
-  val eol: String = sys.props("line.separator")
 
-  def border(cols: Int): String = "  X" + "  X" * cols + "  X" + eol
+object Main:
+  def main(args: Array[String]): Unit =
+    val field = new Field(5, 5, Filled.Empty)
+    println(field)
+    loop(field, 2)
+    
+  
+  def loop(field: Field, loops: Int): Unit =
+    loops match 
+      case x if x > 0 =>
+        println("Eingabe")
+        val input = readLine()
+        val chars = input.toCharArray
+        val x = chars(0)
+        val y = chars(1)
+        val updatedField: Field = field.putShip(x, y.toString)
+        val updatedLoop = loops - 1
+        println(updatedField)
+        loop(updatedField, updatedLoop)
+      case 0 =>
+        println("Spiel beendet.")
+      
 
-  def letters(cols: Int): String = {
-    val letters = ('A' until ('A' + cols).toChar).map(_.toString).mkString("  ")
-    s"     $letters" + eol
-  }
-
-  def generateBoard(rows: Int, cols: Int): String = {
-    val rowIndices = (0 until rows).map(i => (i + 1).toString)
-    val gameBoardRows = (0 until rows).map { i =>
-      (0 until cols).map(row => "  -").mkString("X", "", "  X")
-    }
-    val boardWithIndices = rowIndices
-      .zip(gameBoardRows)
-      .map { case (idx, row) =>
-        s"$idx $row" + eol
-      }
-      .mkString
-    s"$boardWithIndices"
-  }
-
-  def printBoard(): String =
-    letters(cols) + border(cols) + generateBoard(rows, cols) + border(cols)
-}
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    val gameBoard = GameBoard(rows = 5, cols = 5)
-    println("Welcome to Battleship2 \n")
-    println(gameBoard.printBoard())
-
-  }
-}
+    
---
+++ src/test/scala/de/htwg/se/battleship2/model/FieldSpec.scala
@@ -0,0 +1,60 @@
+package de.htwg.se.battleship2.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class FieldSpec extends AnyWordSpec {
+
+  "A Battleship field" when {
+    "initialized Empty" should {
+      val field1 = new Field(1, 1, Filled.Empty)
+      val field2 = new Field(3, 2, Filled.Empty)
+      val field3 = new Field(3, 3, Filled.Empty)
+
+      "have a bar as String of form '+---+---+" in {
+        field2.bar() should be("+---+---+\n")
+      }
+
+      "have a scalable bar" in {
+        field1.bar(1, 1) should be("+-+\n")
+        field2.bar(2, 1) should be("+--+\n")
+        field3.bar(2, 2) should be("+--+--+\n")
+      }
+       "have cells as String of form '| - | - | - | 1'" in {
+        field2.cells(0, 1) should be("|  -  |  -  |  -  | 1\n")
+        
+      }
+
+      "have scalable cells" in {
+        field2 .cells(0, 1) should be("| - | 1\n")
+        field2.cells(0, 2) should be("| - | - | 1\n")
+        field2.cells(1, 2) should be("| - | - | 2\n")
+        
+      }
+       "have a mesh in form " +
+                "+---+---+---+" +
+                "| - | - | - |" +
+                "+---+---+---+" +
+                "| - | - | - |" +
+                "+---+---+---+" in {
+                field2.mesh(3) should be(
+                    "+---+---+---+\n" +
+                    "| - | - | - |\n" +
+                    "+---+---+---+\n" +
+                    "| - | - | - |\n" +
+                    "+---+---+---+\n")
+                field1.mesh(1) should be("+-+\n" + "|-|\n" + "+-+\n")
+                field1.mesh() should be("+-------+\n" + "|   -   |\n" + "|   -   |\n" + "+-------+\n")
+                field1.rowSize should be(1)
+                field3.mesh() should be(
+                    "+-------+-------+-------+\n" +
+                    "|   -   |   -   |   -   |\n" +
+                    "+-------+-------+-------+\n" +
+                    "|   -   |   -   |   -   |\n" +
+                    "+-------+-------+-------+\n" +
+                    "|   -   |   -   |   -   |\n" +
+                    "+-------+-------+-------+\n")
+            }
+   }
+ }
+}
